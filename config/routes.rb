d76973ad239f948b4d774f8c3114d35a7b07553f
nora von breitenbach <noravonbreitenbach@gmail.com>
2021-05-25 16:52:44 +0200

    start homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'dog/controller'
-  resources :index_controllers
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
